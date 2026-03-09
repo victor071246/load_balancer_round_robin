8195afc91da0f10072cbddd24452aaf2d900f578
v <daderiovictorfelipe@gmail.com>
2026-03-08 23:42:24 -0300

    servidores espantalhos rodando'

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 mod dummy_servers;
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    tokio::spawn(dummy_servers::get_servers_up(3001, "Servidor A"));
+    tokio::spawn(dummy_servers::get_servers_up(3002, "Servidor B"));
+    tokio::spawn(dummy_servers::get_servers_up(3003, "Servidor C"));
+
+    println!("Dummy servers running on port 3001, 3002 and 3003");
+    println!("Load balancer running on port 8080");
+
+    tokio::signal::ctrl_c().await.unwrap();
+    println!("Finishing...")
 }
